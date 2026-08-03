4ab8547affa26056cbf63a4977403824611cfeba
Erik Aouizerate <erik.aouizerate.pro@protonmail.com>
2026-08-03 14:49:52 +0200

    feat: scaffold NestJS API workspace with health endpoint

---
+++ api/jest.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from 'jest';
+
+export default {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: { '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+} satisfies Config;
